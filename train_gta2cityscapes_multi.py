b2488c0a266f492acf3a5e60d1b18f008d6e30ba
sahilkrishna <sahilkrishna1243@gmail.com>
2025-01-26 19:48:03 +0500

    Update training code

--- train_gta2cityscapes_multi.py
+++ train_gta2cityscapes_multi.py
@@ -408,8 +408,13 @@ def main():
             loss_D1.backward()
             loss_D2.backward()
 
-            loss_D_value1 += loss_D1.data.cpu().numpy()[0]
-            loss_D_value2 += loss_D2.data.cpu().numpy()[0]
+            # loss_D_value1 += loss_D1.data.cpu().numpy()[0]
+            # loss_D_value2 += loss_D2.data.cpu().numpy()[0]
+
+            loss_D_value1 += loss_D1.item()
+            loss_D_value2 += loss_D2.item()
+
+
 
         optimizer.step()
         optimizer_D1.step()
